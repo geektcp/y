15989cc862d0a48dd8cb0842c65242d21d2d93bf
geektcp <geektcp@gmail.com>
2023-10-09 19:52:43 +0800

    review

--- app/main.cpp
+++ app/main.cpp
@@ -12,6 +12,9 @@
 #include "exampleConfig.h"
 #include "example.h"
 
+using namespace std;
+
+
 /*
  * Simple main program that demontrates how access
  * CMake definitions (here the version number) from source code.
@@ -28,8 +31,13 @@ int main() {
             << std::endl;
   std::system("df -h");
 
+  cout<<__cplusplus<<endl;
+
   // Bring in the dummy class from the example source,
   // just to show that it is accessible from main.cpp.
   Dummy d = Dummy();
-  return d.doSomething() ? 0 : -1;
+  int ret = d.doSomething() ? 0 : -1;
+  std::cout << "ret: " << ret
+          << std::endl;
+  return ret;
 }
