c13fb6bb5f7565b6c393db5fd02744e2b2c505e0
geektcp <geektcp@gmail.com>
2023-11-17 15:32:22 +0800

    hello world

--- app/para/example_para.cpp
+++ app/para/example_para.cpp
@@ -7,7 +7,6 @@
 #include "doctest/doctest.h"
 #endif
 
-#include "Common.h"
 #include "HttpRequest.h"
 #include <iostream>
 #include <stdexcept>
@@ -29,37 +28,16 @@ void crash2222 ()
         x[i] += 3.0 * i;
 }
 
-//void handle_eptr(std::exception_ptr eptr) // passing by value is ok
-//{
-//    try {
-//        if (eptr) {
-//            std::rethrow_exception(eptr);
-//        }
-//    } catch(const std::exception& e) {
-//        std::cout <<"Caught exception "" << e.what() <<";
-//        std::cout<< boost::stacktrace::stacktrace() <<std::endl;
-//    }
-//}
-/*
- */
 int main() {
     ::signal(SIGSEGV, &print_stacktrace);
     ::signal(SIGABRT, &print_stacktrace);
 
-//    spdlog::info("test httpclient {} !", "any api");
+//    spdlog::info("test para {} !", "any api");
 
 //    crash2222();
 
-
-//    try {
-//        throw 11;
-//    } catch (const std::exception& exc) {
-//        eptr = std::current_exception(); // capture
-//    }
-//    handle_eptr(eptr);
     HttpRequest httpRequest;
-//    httpRequest.request();
-    httpRequest.rest();
+    httpRequest.RequestDirect("http://postman-echo.com/headers");
 
     return 0;
 }
